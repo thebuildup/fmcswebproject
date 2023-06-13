a454ed30663aaaecc098fd92b27e200e12c2d04c
Roman Rudakov <ramanrudakou@gmail.com>
2023-06-13 16:07:16 +0300

    Initial commit

---
+++ fmcs/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+# Create your views here.
+
+
+def index(request):
+    # return HttpRequest("<h4>Hello</h4>")
+    return render(request, 'main/index.html')
