6170e705b753d6e4c4c6ee7972fcb7b199ed5808
Roman Rudakov <ramanrudakou@gmail.com>
2023-07-10 10:29:07 +0300

    Исправлена фильтрация ивентов в explore.html и добавлен reverse_event_list в views.py

--- fmcs/leaderboard/views.py
+++ fmcs/leaderboard/views.py
@@ -6,10 +6,11 @@ from .models import Event, Organizator
 # Create your views here.
 def event_view(request):
     events_list = Event.objects.all()
+    reversed_events_list = [event for event in reversed(events_list)]
     org_list = Organizator.objects.all()
     context = {
-        'events_list': events_list,
-        'org_list': org_list
+        'events_list': reversed_events_list,
+        'org_list': org_list,
     }
 
     return render(request, 'leaderboard/explore.html', context=context)
