c88c7a2c1618080cd8690670e561167fcb15754d
thebuildup <39786860+thebuildup@users.noreply.github.com>
2023-08-14 08:14:30 +0300

    feature($teamsapp): Testing

--- fmcs/team/views.py
+++ fmcs/team/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from glicko.models import Player
+from django.urls import reverse
+from django.template.defaultfilters import slugify
 
 
 # Create your views here.
-def team_profile(request):
-    return render(request, 'teams/team_profile.html')
+def team_profile(request, player_id):
+    player = Player.objects.get(pk=player_id)
+
+    # Create a player link in the format: playername-id
+    player_link = f"{slugify(player.name)}-{player_id}"
+
+    context = {
+        'player': player,
+        'player_link': player_link,
+    }
+
+    return render(request, 'teams/team_profile.html', context)
