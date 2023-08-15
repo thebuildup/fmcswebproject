912d565753eb528279ebbda418e98e6c8519c050
Roman Rudakov <ramanrudakou@gmail.com>
2023-08-15 15:54:06 +0300

    feature($teamprofile): Completed Team Rating Chart
    - Added ratingchart.js
    - Edited chart.css, team_profile.py, views.py

---
+++ fmcs/static/js/ratingchart.js
@@ -0,0 +1,146 @@
+// Wait for the DOM to load and then render the chart
+document.addEventListener("DOMContentLoaded", function() {
+  const seriesDataElement = document.getElementById("area-chart");
+  const series = JSON.parse(seriesDataElement.getAttribute("data-series"));
+  const categories = JSON.parse(seriesDataElement.getAttribute("data-categories"));
+  renderChart(series, categories);
+});
+
+function renderChart(series, categories) {
+    /* Get CSS Variables */
+    const colorPrimary = getComputedStyle(document.documentElement)
+      .getPropertyValue("--color-primary")
+      .trim();
+
+    const colorDefault = getComputedStyle(document.documentElement)
+      .getPropertyValue("--color-default")
+      .trim();
+
+    const colorLabel = getComputedStyle(document.documentElement)
+      .getPropertyValue("--color-label")
+      .trim();
+
+    const fontFamily = getComputedStyle(document.documentElement)
+      .getPropertyValue("--font-family")
+      .trim();
+
+    /* Declare Default Chart Options */
+
+    const defaultOptions = {
+      chart: {
+        toolbar: {
+          show: false,
+        },
+        selection: {
+          enabled: false,
+        },
+        zoom: {
+          enabled: false,
+        },
+        width: "100%",
+        height: 180,
+        offsetY: 8,
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      legend: {
+        show: false,
+      },
+      states: {
+        hover: {
+          filter: {
+            type: "none",
+          },
+        },
+      },
+    };
+
+    // Bar Chart
+
+    var barOptions = {
+      ...defaultOptions,
+      chart: {
+        ...defaultOptions.chart,
+        type: "area",
+      },
+      tooltip: {
+        enabled: true,
+        fillSeriesColor: false,
+        style: {
+          fontFamily: fontFamily,
+        },
+        y: {
+          formatter: (value) => {
+            return `${value}`;
+          },
+        },
+      },
+      series: [
+        {
+          name: series[0].name,
+          data: series[0].data,
+        },
+      ],
+      colors: [colorPrimary],
+      fill: {
+        type: "gradient",
+        gradient: {
+          type: "vertical",
+          opacityFrom: 1,
+          opacityTo: 0,
+          stops: [0, 100],
+          colorStops: [
+            [
+              {
+                offset: 0,
+                color: "#ffffff",
+                opacity: 0.2,
+              },
+              {
+                offset: 100,
+                color: "#ffffff",
+                opacity: 0,
+              },
+            ],
+          ],
+        },
+      },
+      stroke: { colors: [colorPrimary], lineCap: "round" },
+      grid: {
+        borderColor: "rgba(0, 0, 0, 0)",
+        padding: { left: 10, right: 0, top: -16, bottom: -8 },
+      },
+      markers: {
+        strokeColors: colorPrimary,
+      },
+      yaxis: {
+//        show: false,
+        show: true,
+      },
+      xaxis: {
+        labels: {
+          floating: true,
+          show: true,
+          style: {
+            fontFamily: fontFamily,
+            colors: colorLabel,
+          },
+        },
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+        crosshairs: {
+          show: false,
+        },
+        categories: categories,
+      },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#area-chart"), barOptions);
+
+    chart.render();
+}
--- fmcs/team/views.py
+++ fmcs/team/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render
-from glicko.models import Player
+from glicko.models import Player, PlayerRatingNode, RatingPeriod
 from django.urls import reverse
 from django.template.defaultfilters import slugify
+import json
 
 
 # Create your views here.
 def team_profile(request, player_id):
     player = Player.objects.get(pk=player_id)
 
-    # Create a player link in the format: playername-id
-    # player_link = f"{slugify(player.name)}-{player.id}"
+    # player_ratings = PlayerRatingNode.objects.filter(player_id=player_id).order_by('-rating_period__end_datetime')[:12]
+    player_ratings = PlayerRatingNode.objects.filter(player_id=player_id).order_by('-rating_period__end_datetime')
+
+    ratings_data = [
+        round(rating.rating) for rating in player_ratings
+    ]
+
+    reversed_ratings_data = list(reversed(ratings_data))
+
+    rating_periods = RatingPeriod.objects.all().order_by('-end_datetime')[:len(reversed_ratings_data)]
+    categories = [rp.end_datetime.strftime("%b") for rp in rating_periods]
+    reversed_categories = list(reversed(categories))
 
     context = {
         'player': player,
+        'series': json.dumps([
+            {
+                'name': 'Rating',
+                'data': reversed_ratings_data,
+            },
+        ]),
+        'categories': json.dumps(reversed_categories),
     }
 
     return render(request, 'teams/team_profile.html', context)
