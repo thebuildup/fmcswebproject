95b9399888eee53b6cf8272baf49e038bb5eebfe
thebuildup <39786860+thebuildup@users.noreply.github.com>
2023-08-16 22:46:17 +0300

    fix($ranking-search): Added another variations in ranking_search.js

--- fmcs/static/js/ranking_search.js
+++ fmcs/static/js/ranking_search.js
@@ -13,10 +13,12 @@ document.addEventListener('DOMContentLoaded', function () {
     // Обработчик события для фильтрации по keyword
     document.getElementById('searchInput').addEventListener('input', function () {
         var keyword = this.value;
-
+        console.log(keyword)
         // Отправляем AJAX-запрос на сервер с параметром keyword
         var xhr = new XMLHttpRequest();
+        console.log(xhr)
         var url = '/ranking/search/?keyword=' + encodeURIComponent(keyword);
+        console.log(url)
         xhr.open('GET', url, true);
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr.onreadystatechange = function () {
@@ -27,3 +29,34 @@ document.addEventListener('DOMContentLoaded', function () {
         xhr.send();
     });
 });
+//document.addEventListener('DOMContentLoaded', function () {
+//    // Обработчик события для фильтрации по keyword
+//    document.getElementById('searchInput').addEventListener('input', function () {
+//        var keyword = this.value;
+//        console.log(keyword)
+//        // Отправляем AJAX-запрос на сервер с параметром keyword
+//        var xhr = new XMLHttpRequest();
+//        console.log(xhr)
+//        var url = '/ranking/search/?keyword=' + encodeURIComponent(keyword);
+//        console.log(url)
+//        xhr.open('GET', url, true);
+//        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+//        xhr.onreadystatechange = function () {
+//            if (xhr.readyState === 4 && xhr.status === 200) {
+//                document.getElementById('search-results').innerHTML = xhr.responseText;
+//            }
+//        };
+//        xhr.send();
+//    });
+//
+//    // Запрос на отображение всех участников при открытии страницы
+//    var xhr = new XMLHttpRequest();
+//    xhr.open('GET', '/ranking/search/', true);
+//    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+//    xhr.onreadystatechange = function () {
+//        if (xhr.readyState === 4 && xhr.status === 200) {
+//            document.getElementById('search-results').innerHTML = xhr.responseText;
+//        }
+//    };
+//    xhr.send();
+//});
