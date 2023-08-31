097b744ae857f8efe87c948f1325838bc1b96050
thebuildup <ramanrudakou@gmail.com>
2023-08-31 11:44:57 +0300

    refactor(URLs): Changed URL name for FMCS Ranking

--- fmcs/glicko/urls.py
+++ fmcs/glicko/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ranking_view, name='ranking'),
+    path('', views.ranking_view, name='fmcs'),
     path('search/', views.search_players, name='search_players'),
 ]
