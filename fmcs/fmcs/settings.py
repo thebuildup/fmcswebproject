0251903afdf35fde030ee9cc3a706dd7077fceb0
thebuildup <39786860+thebuildup@users.noreply.github.com>
2023-09-02 23:02:05 +0300

    fix(EditProfilePage): fix

--- fmcs/fmcs/settings.py
+++ fmcs/fmcs/settings.py
@@ -137,10 +137,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = "/static/"
-STATIC_ROOT = "/home/fmchamps/public_html/fmcs/static"
-STATIC_URL = "/fmcs/static/"
+STATIC_ROOT = "/home/fmchamps/public_html/fmcs/media"
+STATIC_URL = "/fmcs/media/"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+MEDIA_ROOT = "/home/fmchamps/public_html/fmcs/static"
+MEDIA_URL = "/fmcs/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
