80d2147e5c46540fa4edc55c46dd1e1441074872
thebuildup <39786860+thebuildup@users.noreply.github.com>
2023-09-03 23:18:39 +0300

    feature(owl): edit

--- fmcs/users/urls.py
+++ fmcs/users/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('<str:username>/', views.profile, name='user_profile'),
-    path('/edit/', views.edit_profile, name='edit_profile'),
+    path('edit', views.edit_profile, name='edit_profile'),
 ]
