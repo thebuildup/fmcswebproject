7f28ba9ab51bf6bd5753c216ba1d4c5e1dacd2f2
thebuildup <39786860+thebuildup@users.noreply.github.com>
2023-09-12 18:27:00 +0300

    DRF Test

--- fmcs/glicko/urls.py
+++ fmcs/glicko/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.ranking_view, name='fmcs'),
     path('search/', views.search_players, name='search_players'),
-    path('players/', views.PlayerList.as_view())
+    path('players/', views.PlayerViewSet.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
